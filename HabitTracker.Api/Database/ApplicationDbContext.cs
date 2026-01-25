fecbafa4a058c941d45588bd08925e8a5a6227a7
FernPascoal <fernando.pascoal42@gmail.com>
2026-01-25 13:47:43 -0300

    feat: add application db context

---
+++ HabitTracker.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using HabitTracker.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HabitTracker.Api.Database;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : DbContext(options)
+{
+    public DbSet<Habit> Habits { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Application);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
